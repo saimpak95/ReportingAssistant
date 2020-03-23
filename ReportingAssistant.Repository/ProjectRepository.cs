ccfaa327ab3e7a2f435473ab3bd58667737af895
saimpak95 <46064978+saimpak95@users.noreply.github.com>
2020-03-23 14:52:57 +0500

    ProjectRepository DONE

--- ReportingAssistant.Repository/ProjectRepository.cs
+++ ReportingAssistant.Repository/ProjectRepository.cs
@@ -1,12 +1,60 @@
-﻿using System;
+﻿using ReportingAssistant.DomainModel;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ReportingAssistant.Repository
 {
-    public class ProjectRepository
+    public interface IProjectRepository
     {
+        void InsertProject(Projects projects);
+        void UpdateProject(Projects projects);
+        void DeleteProjects(int ProjectID);
+        List<Projects> GetProject();
+        Projects GetProjectByID(int ProjectID);
+    }
+    public class ProjectRepository : IProjectRepository
+    {
+        ReportingAssistantDBContext db;
+        public ProjectRepository()
+        {
+            db = new ReportingAssistantDBContext();
+        }
+        public void DeleteProjects(int ProjectID)
+        {
+            Projects project= db.projects.Where(temp => temp.ProjectID == ProjectID).FirstOrDefault();
+            db.projects.Remove(project);
+            db.SaveChanges();
+        }
+
+        public List<Projects> GetProject()
+        {
+            List<Projects> projects = db.projects.OrderByDescending(temp => temp.ProjectID).ToList();
+            return projects;
+        }
+
+        public Projects GetProjectByID(int ProjectID)
+        {
+            Projects project = db.projects.Where(temp => temp.ProjectID == ProjectID).FirstOrDefault();
+            return project;
+        }
+
+        public void InsertProject(Projects projects)
+        {
+            db.projects.Add(projects);
+            db.SaveChanges();
+        }
+
+        public void UpdateProject(Projects projects)
+        {
+            Projects ExistingProject = db.projects.Where(temp => temp.ProjectID == projects.ProjectID && temp.AdminID==projects.AdminID).FirstOrDefault();
+            ExistingProject.ProjectName = projects.ProjectName;
+            ExistingProject.AvailabilityStatus = projects.AvailabilityStatus;
+            ExistingProject.CategoryID = projects.CategoryID;
+            ExistingProject.AdminID = projects.AdminID;
+            ExistingProject.Image = projects.Image;
+            db.SaveChanges();
+        }
+
+      
     }
 }
