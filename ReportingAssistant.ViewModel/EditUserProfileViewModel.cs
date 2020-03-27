b5a99f1302eac1ab551e3ce159f981c2af8f4e81
saimpak95 <46064978+saimpak95@users.noreply.github.com>
2020-03-27 17:35:05 +0500

    EditUserProfileViewModel DONE

---
+++ ReportingAssistant.ViewModel/EditUserProfileViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReportingAssistant.ViewModel
+{
+    public class EditUserProfileViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [RegularExpression(@"(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})")]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(11, ErrorMessage = "Maximum Length is 11")]
+        [MinLength(11, ErrorMessage = "Minimum Length is 11")]
+        public string Phone { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+    }
+}
