af6cc01bbe4e470266ca8bcf1cee39c0a2914557
saimpak95 <46064978+saimpak95@users.noreply.github.com>
2020-03-27 20:41:36 +0500

    DeleteTaskDoneViewModel DONE

---
+++ ReportingAssistant.ViewModel/DeleteTaskDoneViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReportingAssistant.ViewModel
+{
+    public class DeleteTaskDoneViewModel
+    {
+        [Required]
+        public int TaskDoneID { get; set; }
+
+        [Required]
+        public string Screen { get; set; }
+
+        [Required]
+        public string TaskDoneDescription { get; set; }
+
+        [Required]
+        public int UserID { get; set; }
+
+        [Required]
+        public DateTime DateOfTaskDone { get; set; }
+
+        [Required]
+        public string Attachments { get; set; }
+
+        [Required]
+        public int TaskID { get; set; }
+
+        public virtual List<UserViewModel> Users { get; set; }
+
+        public virtual List<TaskViewModel> Tasks { get; set; }
+    }
+}
