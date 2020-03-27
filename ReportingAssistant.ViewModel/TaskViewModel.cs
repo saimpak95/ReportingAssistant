f39786beb3772da17fa19a1ca034acbf9833df39
saimpak95 <46064978+saimpak95@users.noreply.github.com>
2020-03-27 20:22:42 +0500

    TaskViewModel DONE

---
+++ ReportingAssistant.ViewModel/TaskViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReportingAssistant.ViewModel
+{
+    public class TaskViewModel
+    {
+        public int TaskID { get; set; }
+
+        public string Screen { get; set; }
+        public string TaskDescription { get; set; }
+        public int AdminID { get; set; }
+        public int UserID { get; set; }
+        public DateTime DateOfTask { get; set; }
+        public string Attachments { get; set; }
+        public int ProjectID { get; set; }
+
+        public virtual List<ProjectViewModel> Projects { get; set; }
+
+        public virtual List<UserViewModel> Users { get; set; }
+    }
+}
