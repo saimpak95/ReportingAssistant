409870b1a7077d26d810fd26c85595c41f7ba97c
saimpak95 <46064978+saimpak95@users.noreply.github.com>
2020-03-27 20:48:16 +0500

    FinalCommentViewModel

---
+++ ReportingAssistant.ViewModel/FinalCommentViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReportingAssistant.ViewModel
+{
+    public class FinalCommentViewModel
+    {
+        public int FinalCommentID { get; set; }
+
+        public string Screen { get; set; }
+        public string FinalCommentDescription { get; set; }
+        public int UserID { get; set; }
+        public DateTime DateOfFinalComment { get; set; }
+        public string Attachments { get; set; }
+        public int ProjectID { get; set; }
+
+        public virtual List<UserViewModel> Users { get; set; }
+
+        public virtual List<ProjectViewModel> Projects { get; set; }
+    }
+}
