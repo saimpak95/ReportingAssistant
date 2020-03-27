de698b1e3b78569e33f3a23ede1fbffca544b4b1
saimpak95 <46064978+saimpak95@users.noreply.github.com>
2020-03-27 17:59:34 +0500

    ProjectViewModel

---
+++ ReportingAssistant.ViewModel/ProjectViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReportingAssistant.ViewModel
+{
+    public class ProjectViewModel
+    {
+        public string ProjectName { get; set; }
+
+        public DateTime DateOfStart { get; set; }
+
+        public string AvailabilityStatus { get; set; }
+
+        public int CategoryID { get; set; }
+
+        public string Image { get; set; }
+
+        public int AdminID { get; set; }
+
+        public virtual List<CategoriesViewModel> Categories { get; set; }
+
+        public virtual List<UserViewModel> Users { get; set; }
+    }
+}
