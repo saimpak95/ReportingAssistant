3e32e34abcc9ef7267d27326996117ddbac411f5
saimpak95 <46064978+saimpak95@users.noreply.github.com>
2020-03-27 20:39:23 +0500

    NewTaskDoneViewModel & EditTaskDoneViewModel DONE

---
+++ ReportingAssistant.ViewModel/NewTaskDoneViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReportingAssistant.ViewModel
+{
+    public class NewTaskDoneViewModel
+    {
+        [Required]
+        public string Screen { get; set; }
+
+        [Required]
+        public string TaskDoneDescription { get; set; }
+
+        [Required]
+        public int UserID { get; set; }
+
+        [Required]
+        public DateTime DateOfTaskDone { get; set; }
+
+        [Required]
+        public string Attachments { get; set; }
+
+        [Required]
+        public int TaskID { get; set; }
+
+        public virtual List<UserViewModel> Users { get; set; }
+
+        public virtual List<TaskViewModel> Tasks { get; set; }
+    }
+}
