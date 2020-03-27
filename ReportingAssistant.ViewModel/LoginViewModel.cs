14542d86f3a2c5df1ad0774b477cc3fff20e93ac
saimpak95 <46064978+saimpak95@users.noreply.github.com>
2020-03-27 17:29:34 +0500

    LoginViewModel DONE

---
+++ ReportingAssistant.ViewModel/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReportingAssistant.ViewModel
+{
+   public class LoginViewModel
+    {
+
+        [Required]
+        [RegularExpression(@"(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
--- ReportingAssistant.ViewModel/RegisterViewModel.cs
+++ ReportingAssistant.ViewModel/RegisterViewModel.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ReportingAssistant.ViewModel
 {
-   public class RegisterViewModel
+    public class RegisterViewModel
     {
-
         [Required]
         public string UserName { get; set; }
 
@@ -19,17 +13,17 @@ namespace ReportingAssistant.ViewModel
 
         [Required]
         public string Password { get; set; }
+
         [Required]
         [Compare("Password")]
         public string ConfirmPassword { get; set; }
 
         [Required]
-        [MaxLength(11,ErrorMessage ="Maximum Length is 11")]
+        [MaxLength(11, ErrorMessage = "Maximum Length is 11")]
         [MinLength(11, ErrorMessage = "Minimum Length is 11")]
         public string Phone { get; set; }
 
         [Required]
         public string Gender { get; set; }
-
     }
 }
