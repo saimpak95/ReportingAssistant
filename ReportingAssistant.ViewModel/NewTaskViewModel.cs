b9d8a239263eee178b9e35352c2b3eef6e568c83
saimpak95 <46064978+saimpak95@users.noreply.github.com>
2020-03-27 20:25:41 +0500

    NewTaskViewModel DONE

---
+++ ReportingAssistant.ViewModel/NewTaskViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReportingAssistant.ViewModel
+{
+    public class NewTaskViewModel
+    {
+        [Required]
+        public string Screen { get; set; }
+
+        [Required]
+        public string TaskDescription { get; set; }
+
+        [Required]
+        public int AdminID { get; set; }
+
+        [Required]
+        public int UserID { get; set; }
+
+        [Required]
+        public DateTime DateOfTask { get; set; }
+
+        [Required]
+        public string Attachments { get; set; }
+
+        [Required]
+        public int ProjectID { get; set; }
+
+        public virtual List<ProjectViewModel> Projects { get; set; }
+
+        public virtual List<UserViewModel> Users { get; set; }
+    }
+}
