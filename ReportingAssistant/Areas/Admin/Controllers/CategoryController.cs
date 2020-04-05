a731c8cc78ce19b54477acba253ce1dd32e78093
saimpak95 <46064978+saimpak95@users.noreply.github.com>
2020-04-05 17:23:13 +0500

    Categories DONE

---
+++ ReportingAssistant/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,73 @@
+﻿using ReportingAssistant.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ReportingAssistant.ServiceLayer;
+using ReportingAssistant.DomainModel;
+
+namespace ReportingAssistant.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        ICategoryService cs;
+        public CategoryController(ICategoryService cs)
+        {
+            this.cs = cs;
+        }
+        public ActionResult Index()
+        {
+            List<CategoriesViewModel> cvm = cs.GetCategory();
+            return View(cvm);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(NewCategoriesViewModel ncvm)
+        {
+            if (ModelState.IsValid)
+            {
+                this.cs.InsertCategory(ncvm);
+            }
+            return RedirectToAction("Index", "Category", new { area = "Admin" });
+        }
+        public ActionResult Edit(int ID)
+        {
+            CategoriesViewModel cvm = this.cs.GetCategoryByID(ID);
+            return View(cvm);
+
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EditCategoryViewModel ecvm)
+        {
+            this.cs.UpdateCategory(ecvm);
+            return RedirectToAction("Index", "Category", new { area = "Admin" });
+
+        }
+
+        public ActionResult Delete(int ID)
+        {
+            CategoriesViewModel cvm = this.cs.GetCategoryByID(ID);
+            return View(cvm);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int ID, CategoriesViewModel csm)
+        {
+            this.cs.DeleteCategory(ID);
+            return RedirectToAction("Index", "Category", new { area = "Admin" });
+        }
+
+        public ActionResult Detail(int ID)
+        {
+            CategoriesViewModel cvm = this.cs.GetCategoryByID(ID);
+            return View(cvm);
+        }
+    }
+}
